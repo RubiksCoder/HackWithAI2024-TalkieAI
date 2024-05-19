7e88853f12e8d2a2928f0d6b7fe484d7fc35ec9d
andy-buk <andy.bukovcan@gmail.com>
2024-05-19 13:51:13 -0400

    working app

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import os
 import gradio as gr
 import assemblyai as aai
-
+import os
+import google.generativeai as genai
 
 load_dotenv()
 
@@ -15,9 +16,9 @@ headers = {
 }
 
 data = {
-  "phone_number": "+16136129319",
+  "phone_number": "",
   "from": None,
-  "task": "You are Sammy. You are a personal assistant for Yash Joshi. You are calling TD bank to ask about how to open a tax free savings account.",
+  "task": "",
   "model": "enhanced",
   "language": "eng",
   "voice": "Florian",
@@ -39,23 +40,64 @@ data = {
   "webhook": None
 }
 
-# requests.post('https://api.bland.ai/v1/calls', json=data, headers=headers)
+# CONFIGURE AI:
+assembly_api_key = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=assembly_api_key)
 
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash-latest",
+  generation_config=generation_config,
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
+def generate_output(message, name):
+  print(message)
+  default = f"""
+    You are Joe. You are a personal assistant for {name}. 
+  """
+  before = f"""
+
+  """
+  response = chat_session.send_message(before + message)
+  print(default + response.text)
+  return default + response.text
 
 def process_input(name, phone_number, audio, text):
-  transcriber = aai.Transcriber()
-  transcript = transcriber.transcribe(audio)
+  # IF WE HAVE AUDIO:
+  if audio is not None:
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio)
 
-  if transcript.status == aai.TranscriptStatus.error:
-    print(transcript.error)
+    if transcript.status == aai.TranscriptStatus.error:
+      print(transcript.error)
+    else:
+      print(transcript.text)
+      # This is where you would process the inputs and generate an output
+    msg = generate_output(transcript.text + f"name is: {name} and phone number to call: {phone_number}", name)
+    data["phone_number"] = phone_number
+    data["task"] = msg
+    requests.post('https://api.bland.ai/v1/calls', json=data, headers=headers)
+  
+  # IF THERE IS NO AUDIO:
   else:
-    print(transcript.text)
-    # This is where you would process the inputs and generate an output
-  return transcript.text + f"name is: {name} and phone number to call: {phone_number}"
-
+    msg = generate_output(text + f"name is: {name} and phone number to call: {phone_number}", name)
+    data["phone_number"] = phone_number
+    data["task"] = msg
+    requests.post('https://api.bland.ai/v1/calls', json=data, headers=headers)
 
 def main():
-
   # Define the interface
   iface = gr.Interface(
       process_input,
