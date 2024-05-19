ae33ea1679b49c3bd90ea3fbc775596f788c6fd5
Yash Joshi <y6joshi@uwaterloo.ca>
2024-05-19 11:48:24 -0400

    added assembly ai transcription

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import requests
 from dotenv import load_dotenv
 import os
 import gradio as gr
+import assemblyai as aai
+
 
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
+# Replace with your API key
+aai.settings.api_key = os.getenv('ASSEMBLY_AI_API_KEY')
 
 headers = {
    'Authorization': f'sk-{api_key}'
@@ -14,7 +18,7 @@ headers = {
 data = {
   "phone_number": "+16136129319",
   "from": None,
-  "task": "Your name is Sarah, and you’re a surveyor working on behalf of a small business directory. You joined the company earlier this year. The company’s goal is to collect data about small businesses and create a proprietary dataset to sell to private equity funds. Your job is to ask four basic questions, and collect the information from that small business.\n\nThe four questions you need to answer are:\n1. The business’ category and what services it provides\n2. The business’ hours of operation\n3. The business’ size (how many employees and customers it has)\n4. The owner’s contact information\n\nHere’s an example dialogue\nPerson: Hello this is Squaw Valley Plumbing Co, my name is Jessica, how can I help you?\nYou: Hi Jessica, this is Sarah, I’m calling on behalf of a local small business directory. I wanted to create a listing for your company - do you have time to help?\nPerson: Yeah absolutely. Just to make sure though, you’re making a local directory? What do you need to know?\nYou: Yes, we collect this information on a semi-annual basis to understand the state and overall health of small businesses in the valley. I just have a list of questions to go through.\nPerson: Sounds good, go for it.\nYou: Awesome. First question is: what services do you all provide to the community?\nPerson: We provide plumbing services. Most of the time it’s folks calling in because they have an issue with their sink or toilet. You know how it is.\nYou: Right, yeah. Second question is: what are your hours of operations?\nPerson: Monday through Saturday it’s 9am-7pm. And then Sundays it’s 10am-2pm.\nYou: Do you observe federal holidays?\nPerson: Yes of course.\nYou: Okay, perfect. And at this point, could you give me a sense of how long you’ve been serving our community for?\nPerson: We opened up shop about ten years ago. Feels like we’ve been in the valley forever.\nYou: Haha I’m sure. And at this point, how large have you all gotten? Could you give me a sense of how many folks you’re currently employing?\nPerson: Yeah we’ve gotten pretty big. We have around 10 plumbers on staff, and then a team of support people working around them.\nYou: Fantastic, that’s great to hear. Last question is: could you share the owner’s contact information with me? We won’t give this info out, but it helps us if we need to follow up and collect more info. \nPerson: Eh, I’m not sure if I’m comfortable doing that\nYou: Yeah no worries, it’s completely up to you. We have a few programs we offer to small businesses in the area, and if you qualify it’s just easier to reach out direct. But again, no worries at all if you don’t feel comfortable sharing.\nPerson: Oh, that’s fine then. The owner’s name is Michael Shelly and his phone number is 8781086645.\nYou: Perfect, thanks so much for your help.\nPerson: Of course! Goodbye.",
+  "task": "You are Sammy. You are a personal assistant for Yash Joshi. You are calling TD bank to ask about how to open a tax free savings account.",
   "model": "enhanced",
   "language": "eng",
   "voice": "Florian",
@@ -39,9 +43,16 @@ data = {
 # requests.post('https://api.bland.ai/v1/calls', json=data, headers=headers)
 
 
-def process_input(audio, text):
+def process_input(name, phone_number, audio, text):
+  transcriber = aai.Transcriber()
+  transcript = transcriber.transcribe(audio)
+
+  if transcript.status == aai.TranscriptStatus.error:
+    print(transcript.error)
+  else:
+    print(transcript.text)
     # This is where you would process the inputs and generate an output
-    return f"Received text: {text}, and audio file of length: {len(audio)} bytes"
+  return transcript.text + f"name is: {name} and phone number to call: {phone_number}"
 
 
 def main():
@@ -50,8 +61,10 @@ def main():
   iface = gr.Interface(
       process_input,
       inputs=[
+          gr.Textbox(label="Name"),
+          gr.Textbox(label="Phone Number to Call:"),
           gr.Audio(type="filepath", label="Speak here"),
-          gr.Textbox(lines=2, placeholder="Type here...", label="Type here")
+          gr.Textbox(lines=2, placeholder="Type here...", label="Prompt here")
       ],
       outputs="text",
       title="Speech and Text Input Demo",
